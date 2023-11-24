57e5fbfed1c694556726e0c3433bb1e0f1c7cb24
yooooj <littleajaygamer@gmail.com>
2023-11-24 16:47:58 -0500

    app.js changes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require("fs");
 const path = require('path');
 
 const app = express();
@@ -6,7 +7,14 @@ const port = 3000;
 
 // Serve static files from the 'src' folder
 app.use(express.static(path.join(__dirname, 'src')));
-
+app.get("/",(req,res)=>{
+    try{
+        res.write(fs.readFileSync("src/index.html"));
+        res.end();
+    }catch{
+        res.write("REFRESH");
+    }
+});
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
