8921db23ae6e97d8edaa2df93be97997eb5f71ec
AmNotAGoose <alexyihengwu@gmail.com>
2023-11-24 15:42:24 -0500

    FIX EVERYTHING HALELEUGIAE

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-const { OpenAI } = require('openai');
+const express = require('express');
+const path = require('path');
 
-require('dotenv').config();
+const app = express();
+const port = 3000;
 
-const apiKey = process.env.OPENAI_KEY;
-const openai = new OpenAI({apiKey: apiKey});
+// Serve static files from the 'src' folder
+app.use(express.static(path.join(__dirname, 'src')));
 
-async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [{"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        {"role": "user", "content": "Where was it played?"}],
-    model: "gpt-3.5-turbo",
-  });
-
-  console.log(completion.choices[0]);
-}
-main();
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
-import OpenAI from "openai";
+const { OpenAI } = require('openai');
 
-const openai = new OpenAI();
+require('dotenv').config();
 
-async function generateSimilarQuestionFunction(question) {
+const apiKey = process.env.OPENAI_KEY;
+const openai = new OpenAI({apiKey: apiKey});
+
+async function main() {
   const completion = await openai.chat.completions.create({
-    messages: [{"role": "system", "content": "You are a helpful assistant that will create similar questions to the one provided."},
-        {"role": "user", "content": "Make one question similar to this one:".concat(question, "Only output the question, do not say anything else.")}],
+    messages: [{"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}],
     model: "gpt-3.5-turbo",
   });
 
   console.log(completion.choices[0]);
 }
-generateSimilarQuestionFunction("Joe has 5 apples. If Amy takes two, how many does he have?");
-
-document.getElementById("")
+main();
