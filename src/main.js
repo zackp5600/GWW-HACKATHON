878b0489bce1c15b55df2225fbc64d3449df6475
Zackp5600 <zackp5600@gmail.com>
2023-11-24 14:09:31 -0500

    chatgpt testing soon

--- src/main.js
+++ src/main.js
@@ -1,21 +1,13 @@
-const OpenAI = require("openai");
-require("dotenv").config();
+import OpenAI from "openai";
 
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-});
+const openai = new OpenAI();
 
-exports.chatReq = async (req, res) => {
-    try {
-      const message = "Which is the capital of Albania?";
-      const response = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: message }],
-        temperature: 0,
-        max_tokens: 1000,
-      });
-      res.status(200).json(response);
-    } catch (err) {
-      res.status(500).json(err.message);
-    }
-  };
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{"role": "system", "content": "what is the meaning of life?"}],
+    model: "gpt-3.5-turbo",
+  });
+
+  console.log(completion.choices[0]);
+}
+main();
