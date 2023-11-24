e538d30881c5ab683227fb063d2ad0d30484f60e
yooooj <littleajaygamer@gmail.com>
2023-11-24 17:06:17 -0500

    added submit btn

--- src/page.js
+++ src/page.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded",()=>{
     for(var i=0;i<document.getElementsByClassName("file").length;i++){
-        
+
     }
+    document.getElementById('new-btn').addEventListener('click',()=>{
+        
+    });
 });
