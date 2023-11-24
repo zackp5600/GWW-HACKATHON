25a5e4230ff00e62bf745c542ae3503c4c3875ec
AmNotAGoose <alexyihengwu@gmail.com>
2023-11-24 15:30:54 -0500

    fixes openai shit

--- app.js
+++ app.js
@@ -1,56 +1,20 @@
-//EXPRESS//
+const { OpenAI } = require('openai');
 
-const express = require("express");
-require("dotenv").config();
-const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config();
 
-const app = express();
+const apiKey = process.env.OPENAI_KEY;
+console.log(apiKey)
+const openai = new OpenAI({apiKey: apiKey});
 
-app.use(express.json());
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}],
+    model: "gpt-3.5-turbo",
+  });
 
-const configuration = new Configuration({
-  apiKey: process.env.OPEN_AI_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
-app.post("/find-complexity", async (req, res) => {
-  try {
-    const { prompt } = req.body;
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `
-              ${prompt}
-      
-              The time complexity of this function is
-              ###
-            `,
-      max_tokens: 64,
-      temperature: 0,
-      top_p: 1.0,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-      stop: ["\n"],
-    });
-
-    return res.status(200).json({
-      success: true,
-      data: response.data.choices[0].text,
-    });
-  } catch (error) {
-    return res.status(400).json({
-      success: false,
-      error: error.response
-        ? error.response.data
-        : "There was an issue on the server",
-    });
-  }
-});
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server listening on port ${port}`));
-
-//LISTEN ON PORT 80//
-app.listen(80,()=>{
-    console.log('listening on port 80');
-});
+  console.log(completion.choices[0]);
+}
+main();
